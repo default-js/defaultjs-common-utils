27a79e474446721c6af6a0d894873a9f73b551b4
Frank Schüler <SchuelerFrank@gmx.de>
2020-10-14 00:09:48 +0200

    update

---
+++ src/Ready.js
@@ -0,0 +1,23 @@
+export default () => {
+	let resolve, reject = null;
+	const result = new Promise((r, e) => {
+		resolve = r;
+		reject = e;
+	});
+
+	result.resolved = false;
+	result.error = false;
+	result.resolve = () => {		
+		result.resolved = true;
+		result.resolve = () => {};
+		resolve();
+	};
+	result.reject = () => {
+		result.resolved = true;
+		result.error = true;
+		result.reject = () => {};
+		reject();
+	};
+
+	return result;
+};
